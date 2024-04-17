85d33214c7b005a3744ccba0ca402e8169b00b7f
CamMilot <148920550+CamMilot@users.noreply.github.com>
2024-04-17 18:49:12 -0400

    Added Item Page and Cleaned Code

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ cursor.execute(f'SELECT * FROM {tableName}')
 result = cursor.fetchall()
 
 
+columnNames = [desc[0] for desc in cursor.description] 
 
 
 
@@ -30,32 +31,67 @@ templates = Jinja2Templates(directory="templates")
 linebreaks = Markup('<br>'*10)
 
 
+items = pd.DataFrame(result)
+items.columns = columnNames
 
-@app.get("/")
-def read_root(request: Request):
+
+
+
+def createDivHome(items,request):
     div = ''
-    for row in result:
-        item = row[1]
-        price = row[2]
-        link = row[4]
-        sale = row[5]
-        saleAmnt = row[6]
-        priceSale = int(price)*((100-saleAmnt)*.01)
-        price = '{0:.2f}'.format(price)
+    for index,row in items.iterrows():
+        saleAmnt = row['saleamnt']
+        priceSale = int(row['price'])*((100-saleAmnt)*.01)
+        price = '{0:.2f}'.format(row['price'])
         priceSale = '{0:.2f}'.format(priceSale)
-        if bool(sale):
+        if bool(row['sale']):
             price = f'<s> ${price}</s><font color="#8B0000">  ${priceSale} </font> <br> <font color = "gray" size -= 2>{saleAmnt}% Off</font>'
         else:
-            price = '$'+price
+            price = '$'+price 
+        link = row['link']
+        item = row['name']
         inlineCSS = f'style="background-image:url({link})"'
         innerDiv = f'<a class="content" href="{str(request.url)}items/{row[0]}">{item} <br> {price} </a>'   
         innerLink = f'<a class="salelink" href="{str(request.url)}items/{row[0]-1}"></a>'
 
         div+= f'<div class="saleitem" {inlineCSS}>{innerLink}{innerDiv}</div>'
     div = Markup(div)
+    return div
+
+def createDivItem(items,request,itemid):
+    row = items.loc[itemid]
+    div = ''
+    price = row['price']
+    item = row['name']
+    link = row['link']
+    sale = row['sale']
+    saleAmnt = row['saleamnt']
+    priceSale = int(price)*((100-saleAmnt)*.01)
+    price = '{0:.2f}'.format(price)
+    priceSale = '{0:.2f}'.format(priceSale)
+    if bool(sale):
+        price = f'<s> ${price}</s><font color="#8B0000">  ${priceSale} </font> <br> <font color = "gray">{saleAmnt}% Off</font>'
+    else:
+        price = '$'+price
+        
+    inlineCSS = f'style="background-image:url({link})"'
+    innerDiv = f'<div class="content" >{item} <br> {price} </div>'   
+
+    div+= f'<div class="saleitem" {inlineCSS}>{innerDiv}</div>'
+    div = Markup(div)
+    return div
+
+
+
+
+
+@app.get("/")
+def read_root(request: Request):
+    div = createDivHome(items,request)
     return templates.TemplateResponse("main.html",{"request":request,"test":div,"linebreak":linebreaks})
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return result[item_id]
+def read_item(request: Request, item_id: int, q: Union[str, None] = None):
+    div = createDivItem(items,request,item_id)
+    return templates.TemplateResponse("item.html",{"request":request,"test":div,"linebreak":linebreaks})
